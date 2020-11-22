8560659c4d76cf71889fc705c5fa966ed2b19889
alshimaaelnady <alshimaaelnady@gmail.com>
2020-11-22 19:21:39 +0200

    Ui Modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {AppComponent} from './app.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {LoginComponent} from './login/login.component';
+import {HttpInterceptorService} from './services/HttpInterceptorService';
 
-import { AppComponent } from './app.component';
+const routes: Routes = [
+  {path: 'signup' , component : SignUpComponent} ,
+  {path: 'login', component : LoginComponent},
+  {path: '**', redirectTo: '/', pathMatch: 'full'}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent, HeaderComponent, FooterComponent, LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
